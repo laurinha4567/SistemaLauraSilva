a9a1d21a9a055e9cce73278d47f5799caf61f469
laurinha4567 <laura.silva11@estudante.ifms.edu.br>
2026-04-07 15:16:13 -0400

    Criamos o bean e o dao e a tabela de usuarios no bean

---
+++ src/bean/mpvUsuarios.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bean;
+
+import java.util.Date;
+
+/**
+ *
+ * @author u07881974109
+ */
+public class mpvUsuarios {
+    int mpvIdUsuarios;
+    String mpvNome;
+    String mpvApelido;
+    String mpvCpf;
+    Date mpvDataNascimento;
+    int mpvNivel;
+    String mpvSenha;
+    String mpvAtivo;
+    
+    
+}
